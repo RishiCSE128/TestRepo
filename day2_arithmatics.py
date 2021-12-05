429ba5a55296a0f2150557560811fce14180181b
rishiCSE128 <saptarshi.ghosh.cs@gmail.com>
2021-12-05 00:48:21 +0000

    adds arithmatic opt

---
+++ day2_arithmatics.py
@@ -0,0 +1,23 @@
+a = int(input("Enter the first value... "))
+b = int(input("Enter the seconf value... "))
+
+# addition  
+print(f'the sum of {a} and {b} is {a+b}')
+
+# subtraction 
+print(f'the difference of {a} and {b} is {a-b}')
+
+# multiplication
+print(f'the product of {a} and {b} is {a*b}')
+
+# division 
+print(f'the quotent  of {a} and {b} is {a/b}')
+
+# modulus 
+print(f'the remainder of {a} and {b} is {a%b}')
+
+# power
+print(f'the power of {a} over {b} is {a**b}')
+
+# int-div
+print(f'the int-quotent of {a} and {b} is {a//b}')
